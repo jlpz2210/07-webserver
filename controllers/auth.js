801fa334cf97e2746775479b29cf079dbbd14434
jlpz2210 <juanito9897@hotmail.com>
2023-07-19 14:01:29 -0600

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 const { generateJWT } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -44,6 +45,54 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res) => {
+    const {id_token} = req.body;
+
+    try {
+        const { name, picture, email } = await googleVerify(id_token)
+
+        let user = await User.findOne({email})
+        if(!user){
+            const data = {
+                name,
+                email,
+                password: ':P',
+                img: picture,
+                google: true,
+                role: 'USER'
+            }
+            user = new User(data)
+            await user.save()
+        }
+
+        if(!user.isActive){
+            return res.status(401).json({
+                msg: 'User inactive'
+            })
+        }
+
+        const token = await generateJWT(user.id)
+
+
+        res.json({
+            user,
+            token
+        })
+    }catch(error){
+        console.log(error)
+        res.status(400).json({
+            ok: false,
+            msg: "The token couldn't be verified"
+        })
+    }
+
+    res.json({
+        msg: 'Token',
+        id_token
+    })
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const { isAdmin } = require('../middlewares/validate-roles');
@@ -12,5 +12,10 @@ router.post('/login',[
     check('password', 'The password is required').not().isEmpty(),
     validateFields
 ] ,login);
+
+router.post('/google',[
+    check('id_token', 'Token is required').not().isEmpty(),
+    validateFields
+] ,googleSignIn);
 
 module.exports = router;
